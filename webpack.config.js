e8ea94a59ab1916b3b9b8bfd2bed58982906897d
Handaz <ilya.zabolotny2010@gmail.com>
2022-03-09 22:24:48 +0300

    configuring webpack

---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'messanger-[hash].bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+    alias: {
+      handlebars: 'handlebars/dist/handlebars.js',
+    },
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 3000,
+    https: true,
+    hot: true,
+    open: process.env.WEBPACK_SERVER_BROWSER,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          configFile: path.resolve(__dirname, 'tsconfig.json'),
+        },
+        exclude: /(node_modules)/,
+      },
+      {
+        test: /\.module\.(sa|sc|c)ss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+              sourceMap: isDevelopment,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /\.module\.(sa|sc|c)ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      filename: 'index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style-[hash].css',
+    }),
+  ],
+};
